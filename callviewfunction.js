086376fd417c411b49ba1fd95892ac36d9364c1a
Terome Mensah <temens23@tuta.io>
2024-04-05 20:39:32 +0100

    Initial commit

---
+++ callviewfunction.js
@@ -0,0 +1,384 @@
+ABI
+
+
+//// ABI
+const contractABI = [
+
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        },
+        {
+          "name": "arg1",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_0085",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_00A6",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_00EF",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg1",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_010F",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_0142",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "func_0147",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_0173",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "func_046B",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "func_057B",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_059F",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_0682",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        },
+        {
+          "name": "arg1",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_06AB",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        },
+        {
+          "name": "r1",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        },
+        {
+          "name": "arg1",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_06D5",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        },
+        {
+          "name": "arg1",
+          "type": "uint256"
+        },
+        {
+          "name": "arg2",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_06EE",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "arg0",
+          "type": "uint256"
+        },
+        {
+          "name": "arg1",
+          "type": "uint256"
+        }
+      ],
+      "name": "func_0712",
+      "outputs": [
+        {
+          "name": "r0",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "arg0",
+            "type": "uint256"
+          },
+          {
+            "name": "arg1",
+            "type": "uint256"
+          }
+        ],
+        "name": "func_0763",
+        "outputs": [
+          {
+            "name": "r0",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "arg0",
+            "type": "uint256"
+          },
+          {
+            "name": "arg1",
+            "type": "uint256"
+          },
+          {
+            "name": "arg2",
+            "type": "uint256"
+          }
+        ],
+        "name": "func_0785",
+        "outputs": [
+          {
+            "name": "r0",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "arg0",
+            "type": "uint256"
+          },
+          {
+            "name": "arg1",
+            "type": "uint256"
+          }
+        ],
+        "name": "func_07B4",
+        "outputs": [
+          {
+            "name": "r0",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "arg0",
+            "type": "uint256"
+          },
+          {
+            "name": "arg1",
+            "type": "uint256"
+          }
+        ],
+        "name": "func_081E",
+        "outputs": [
+          {
+            "name": "r0",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+    ]
